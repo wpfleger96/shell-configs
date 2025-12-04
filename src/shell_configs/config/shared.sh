513568e006effe6813274f363c20e7d675c42fd7
Will Pfleger <pfleger.will@gmail.com>
2025-12-04 00:06:42 -0500

    add rust config

--- src/shell_configs/config/shared.sh
+++ src/shell_configs/config/shared.sh
@@ -64,6 +64,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+### Rust ###
+[ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
+
 ### Docker ###
 alias docker_cleanup="docker builder prune -af && docker system prune -af"
 
